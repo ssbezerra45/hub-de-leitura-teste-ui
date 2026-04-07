e55e176a547db2b1aede62c0e7d701c159a53a15
sergiobezerra <ssbezerra45@gmail.com>
2026-04-07 19:27:07 -0300

    modificado com sucesso

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 describe('Funcionalidade: Cadastro no Hub de leitura', () => {
     beforeEach(() => {
         cy.visit('register.html')
     });
-    it('Deve fazer cadastro com sucesso', () => {
+    it('Deve fazer cadastro com sucesso usando função Js', () => {
         let email = `sergio${Date.now()}@example.com`
         cy.get('#name').type('Sergio Bezerra')
         cy.get('#email').type(email)
@@ -12,12 +13,22 @@ describe('Funcionalidade: Cadastro no Hub de leitura', () => {
         cy.get('#confirm-password').type('senha123')
         cy.get('#terms-agreement').check()
         cy.get('#register-btn').click()
-        cy.url().should('include', 'dashboard')
-    
-       
-       
-        
+        cy.url().should('include', 'dashboard') 
     });
+     it('Deve fazer cadastro com sucesso usando faker', () => {
+        let telefone = faker.phone.number('119########')
+        let nome =faker.person.fullName()
+        let email = faker.internet.email()
+        cy.get('#name').type(nome)
+        cy.get('#email').type(email)
+        cy.get('#phone').type(telefone)
+        cy.get('#password').type('senha123')
+        cy.get('#confirm-password').type('senha123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        cy.url().should('include', 'dashboard') 
+        cy.get('#user-name').should('contain', nome)
+     });
     
 });
 
